f716f549b06e084e62ba45e634b91ae10078bab3
Ron <niuniuzhu.cn@gmail.com>
2018-09-30 17:39:26 +0800

    use .netstandard2.0

--- Core/r.bat
+++ Core/r.bat
@@ -2,7 +2,3 @@ copy %1%2.dll ..\..\AnimalRunning\Unity\Assets\Dlls /Y
 copy %1%2.pdb ..\..\AnimalRunning\Unity\Assets\Dlls /Y
 copy %1%2.dll ..\..\AnimalRunning\Project\Lib /Y
 copy %1%2.pdb ..\..\AnimalRunning\Project\Lib /Y
-copy %1%2.dll ..\..\Lockstep\Unity\Assets\Dlls /Y
-copy %1%2.pdb ..\..\Lockstep\Unity\Assets\Dlls /Y
-copy %1%2.dll ..\..\Lockstep\Project\Lib /Y
-copy %1%2.pdb ..\..\Lockstep\Project\Lib /Y
